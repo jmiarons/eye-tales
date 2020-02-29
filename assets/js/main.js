5b8618bd4535ed33354f33ebe15dd261c0b5deb8
joelmoff <joelmiarons@gmail.com>
2020-02-29 19:29:19 +0100

    Added image capture every 1s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
-function start() {
-const player = document.getElementById('vid')
+function process() {
+const player = document.getElementById('vid');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 const constraints = {video: true,};
+context.drawImage(player, 0, 0, canvas.width, canvas.height);
 navigator.mediaDevices.getUserMedia(constraints)
 	.then((stream) => {
 		player.srcObject = stream;
 	});
+console.log(canvas.toDataURL());
+}
+
+function start() {
+	window.setInterval(function() {
+		process();
+	}, 1000);
 }
