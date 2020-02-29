3fb444236ef3ef3c6d8557eb640f3f747847038d
Albert Suarez <alsumo95@gmail.com>
2020-02-29 17:09:25 +0000

    Returned mp3 on Flask

--- src/api/describe.py
+++ src/api/describe.py
@@ -1,4 +1,6 @@
-from flask import request
+import io
+
+from flask import request, send_file
 
 from src.helper import response
 from src.text_to_speech import text2speech
@@ -7,9 +9,13 @@ from src.text_to_speech import text2speech
 def post():
     request_body = request.json
     if 'image_base64' not in request_body:
-        return response.make(error=True, message='image_base64 has to be specified.')
+        return response.make(error=True, message='image_base64 has to be specified.', code=400)
     image_base64 = request_body['image_base64']
     # do some magic for get a sentence from the image
-    sentence = 'Test'
+    sentence = 'Thanks Adria, we all love you.'
     audio_binary = text2speech.synthesize_text(sentence)
-    return 'ok', 200
+    return send_file(
+        io.BytesIO(audio_binary),
+        attachment_filename='audio.mp3',
+        mimetype='audio/mpeg'
+    ), 200
