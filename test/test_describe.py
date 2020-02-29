167fc74638410ec59cf96a151b00b51081b565d5
Albert Suarez <alsumo95@gmail.com>
2020-02-29 20:50:06 +0000

    Changed logic.describe test

--- test/test_describe.py
+++ test/test_describe.py
@@ -7,8 +7,9 @@ class DescribeTest(unittest.TestCase):
 
     def setUp(self):
         self.session_id = '123456789'
-        self.image_path = 'examples/test_1.jpg'
+        self.image_path = 'examples/test_4.jpg'
 
     def test_describe(self):
         result = logic.describe(self.session_id, self.image_path)
         self.assertIsNotNone(result)
+        print(result)
