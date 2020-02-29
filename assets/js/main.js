e9a04fa494fd978ec5e08c1ee390db4e3112462e
Albert Suarez <alsumo95@gmail.com>
2020-02-29 23:20:36 +0000

    Implemented API interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
 const player = document.getElementById('vid')
 const button = document.querySelector('.button');
 
-function process() {
+function callApi(sessionId, imageBase64) {
+    var formData = {session_id: sessionId, image_base64: imageBase64};
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost/describe',
+        data: JSON.stringify(formData),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function(data, textStatus, xhr) {
+            console.log(xhr.status);
+        },
+        error: function() {
+            // ignore
+        },
+        complete: function() {
+            // ignore
+        }
+    });
+}
+
+function process(sessionId) {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
     context.drawImage(player, 0, 0, canvas.width, canvas.height);
-    console.log(canvas.toDataURL());
-    // call the api
+    imageBase64 = canvas.toDataURL();
+    imageBase64 = imageBase64.split(',');
+    imageBase64.shift();
+    callApi(sessionId, imageBase64.join());
 }
 
 function start() {
@@ -18,8 +40,9 @@ function start() {
         .then((stream) => {
             player.srcObject = stream;
             button.classList.add('button--hide');
+            const sessionId = (Math.floor(Math.random() * (10000 - 1 + 1)) + 1).toString();
             setInterval(function() {
-                process();
+                process(sessionId);
             }, 1000);
         });
 }
