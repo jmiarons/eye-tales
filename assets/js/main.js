c68f2a2b2c6fbaa063515342d8d1900ce5ba0a33
joelmoff <joelmiarons@gmail.com>
2020-02-29 17:31:07 +0100

    Add Start button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+function start() {
 const player = document.getElementById('vid')
 const constraints = {video: true,};
 navigator.mediaDevices.getUserMedia(constraints)
 	.then((stream) => {
 		player.srcObject = stream;
 	});
+}
