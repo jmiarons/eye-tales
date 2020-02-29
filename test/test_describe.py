4feb8c63813aa7ed4d0577ef4b847ef072293086
Albert Suarez <alsumo95@gmail.com>
2020-02-29 22:09:52 +0000

    Improved tests

--- test/test_describe.py
+++ test/test_describe.py
@@ -1,4 +1,8 @@
+import base64
 import unittest
+import requests
+
+from playsound import playsound
 
 from src.helper import logic
 
@@ -8,8 +12,19 @@ class DescribeTest(unittest.TestCase):
     def setUp(self):
         self.session_id = '123456789'
         self.image_path = 'examples/test_4.jpg'
+        self.endpoint = 'http://localhost:80/describe'
+        self.audio_path = 'data/output.mp3'
+        with open(self.image_path, 'rb') as image_file:
+            self.image_base64 = base64.b64encode(image_file.read()).decode('utf-8')
 
     def test_describe(self):
         result = logic.describe(self.session_id, self.image_path)
         self.assertIsNotNone(result)
         print(result)
+
+    def test_cache(self):
+        data = dict(session_id=self.session_id, image_base64=self.image_base64)
+        response = requests.post(self.endpoint, json=data)
+        with open(self.audio_path, 'wb') as out:
+            out.write(response.content)
+        playsound(self.audio_path)
