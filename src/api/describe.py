6a7ec8123f007488669b9e651060f7323dc4c25b
Albert Suarez <alsumo95@gmail.com>
2020-03-01 00:18:43 +0000

    Added inference to the API + iteration parameter

--- src/api/describe.py
+++ src/api/describe.py
@@ -4,7 +4,9 @@ import random
 
 from flask import request, send_file
 
+from src.eye_tales import max_length, encoder, decoder, image_features_extract_model, tokenizer
 from src.helper import response, logic
+from src.neural_image_caption_generation.inference_image_captioning import evaluate
 from src.text_to_speech import text2speech
 
 
@@ -12,17 +14,22 @@ def post():
     try:
         # Get parameters
         request_body = request.json
-        if 'image_base64' not in request_body or 'session_id' not in request_body:
-            return response.make(True, 'image_base64 and session_id has to be specified.', code=400)
+        if 'image_base64' not in request_body or 'session_id' not in request_body or 'iteration' not in request_body:
+            return response.make(True, 'image_base64, session_id and iteration has to be specified.', code=400)
         image_base64 = request_body['image_base64']
         session_id = request_body['session_id']
+        iteration = request_body['iteration']
         # Save image
         image_path = 'data/{}.jpg'.format(random.choice(range(100)))
         image_encoded = base64.b64decode(image_base64)
         with open(image_path, 'wb') as image_file:
             image_file.write(image_encoded)
-        # Apply logic
-        sentence = logic.describe(session_id, image_path)
+        if iteration % 10 == 0:
+            sentence = evaluate(image_path, max_length, encoder, decoder, image_features_extract_model, tokenizer)
+            sentence = ' '.join(sentence[:-1]).capitalize()
+        else:
+            # Apply logic
+            sentence = logic.describe(session_id, image_path)
         # Transcribe to speech if needed
         if sentence:
             audio_binary = text2speech.synthesize_text(sentence)
--- src/eye_tales.py
+++ src/eye_tales.py
@@ -2,12 +2,15 @@ import connexion
 
 from flask_cors import CORS
 
+from src.neural_image_caption_generation.inference_image_captioning import init
 
 connexion_app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 flask_app = connexion_app.app
 flask_app.config['JSON_AS_ASCII'] = False
 connexion_app.add_api('openapi.yaml', arguments={'title': 'Eye Tales API'})
 CORS(flask_app)
+
+encoder, decoder, max_length, image_features_extract_model, tokenizer = init()
 
 
 @flask_app.route('/')
